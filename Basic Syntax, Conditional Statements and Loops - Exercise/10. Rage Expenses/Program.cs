2b8b05b42fa6bea0e48cd30f9c3274f0e7119043
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-22 22:49:22 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keybardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsetCount = 0;
+            int mouseCount = 0;
+            int keybardCount = 0;
+
+            bool headsetBroken = false;
+            bool mouseBroken = false;
+
+            double price = 0;
+            for (int i = 1; i <= lostGames; i++)
+            {
+                headsetCount++;
+                mouseCount++;
+
+                if (headsetCount == 2)
+                {
+                    headsetBroken = true;
+                    headsetCount = 0;
+                    price += headsetPrice;
+                }
+                else
+                {
+                    headsetBroken = false;
+                }
+                if (mouseCount == 3)
+                {
+                    mouseBroken = true;
+                    mouseCount = 0;
+                    price += mousePrice;
+                }
+                else
+                {
+                    mouseBroken= false;
+                }
+                if (headsetBroken && mouseBroken)
+                {
+                    headsetBroken = false;
+                    mouseBroken = false;
+                    keybardCount++;
+                    price += keybardPrice;
+                }
+                if (keybardCount == 2)
+                {
+                    keybardCount = 0;
+                    price += displayPrice;
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {price:f2} lv.");
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/9. Padawan Equipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _9._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountMoney = double.Parse(Console.ReadLine());
+            int countStudents = int.Parse(Console.ReadLine());
+            double sabers = double.Parse(Console.ReadLine());
+            double robes = double.Parse(Console.ReadLine());
+            double belts = double.Parse(Console.ReadLine());
+
+            double sabersPrice = Math.Ceiling((countStudents * 0.1) + countStudents);
+            double robesPrice = robes * countStudents;
+            double beltsPrice = countStudents / 6;
+
+            double totalPrice = (sabersPrice * sabers) + ((countStudents - beltsPrice) * belts) + robesPrice;
+
+            if (totalPrice <= amountMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+            }
+            else if (amountMoney < totalPrice)
+            {
+                Console.WriteLine($" John will need {totalPrice-amountMoney:f2}lv more.");
+            }
+        }
+    }
+}
