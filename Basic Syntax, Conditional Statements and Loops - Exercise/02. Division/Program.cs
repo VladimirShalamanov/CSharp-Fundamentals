e36744f6f5f52f81e2dcbbb6fd38be4482c488f1
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-22 22:45:39 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/02. Division/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02._Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int divisible = 0;
+            if (num % 10 == 0)
+            {
+                divisible = 10;
+            }
+            else if (num % 7 == 0)
+            {
+                divisible = 7;
+            }
+            else if (num % 6 == 0)
+            {
+                divisible = 6;
+            }
+            else if (num % 3 == 0)
+            {
+                divisible = 3;
+            }
+            else if (num % 2 == 0)
+            {
+                divisible = 2;
+            }
+            if (divisible >= 2)
+            {
+                Console.WriteLine($"The number is divisible by {divisible}");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/1. Ages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int agePerson = int.Parse(Console.ReadLine());
+
+            string word = string.Empty;
+            if (0 <= agePerson && agePerson <= 2)
+            {
+                word = "baby";
+            }
+            else if (3 <= agePerson && agePerson <= 13)
+            {
+                word = "child";
+            }
+            else if (14 <= agePerson && agePerson <= 19)
+            {
+                word = "teenager";
+            }
+            else if (20 <= agePerson && agePerson <= 65)
+            {
+                word = "adult";
+            }
+            else if (agePerson >= 66)
+            {
+                word = "elder";
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
