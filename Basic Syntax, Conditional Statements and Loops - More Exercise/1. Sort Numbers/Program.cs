2abbd5a33e42c83e0999783ec88c11ba7f9e5a32
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-23 23:48:27 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/1. Sort Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int conteiner1 = int.MinValue;
+            int conteiner2 = int.MinValue;
+            int conteiner3 = int.MinValue;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                int numberToChek = int.Parse(Console.ReadLine());
+                if (numberToChek > conteiner1)
+                {
+                    conteiner3 = conteiner2;
+                    conteiner2 = conteiner1;
+                    conteiner1 = numberToChek;
+                }
+                else if (numberToChek > conteiner2)
+                {
+                    conteiner3 = conteiner2;
+                    conteiner2 = numberToChek;
+                }
+                else if (numberToChek > conteiner3)
+                {
+                    conteiner3 = numberToChek;
+                }
+            }
+
+            Console.WriteLine(conteiner1);
+            Console.WriteLine(conteiner2);
+            Console.WriteLine(conteiner3);
+        }
+    }
+}
