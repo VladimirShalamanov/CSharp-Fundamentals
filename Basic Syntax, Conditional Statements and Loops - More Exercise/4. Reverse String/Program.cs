d05561ce9d850e44ebb71108cd0c1df45d573677
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-23 23:49:49 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/4. Reverse String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            string reverse = string.Empty;
+
+            for (int reverseIndex = word.Length - 1; reverseIndex >= 0; reverseIndex--)
+            {
+                reverse += word[reverseIndex];
+            }
+
+            Console.WriteLine(reverse);
+        }
+    }
+}
