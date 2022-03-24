dceb212c52dcf640a97892bf0cceae7789f7c852
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-24 19:29:59 +0200

    Add files via upload

---
+++ Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal PoundsToDollars = 1.31m;
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = pounds * PoundsToDollars;
+            Console.WriteLine($"{Math.Round(dollars, 3 ,MidpointRounding.AwayFromZero):f3}");
+        }
+    }
+}
---
+++ Data Types and Variables - Lab/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int yearsInCenturies = 100;
+            const decimal daysInYear = 365.2422m;
+            const int hoursInDay = 24;
+            const int minutesInHour = 60;
+
+            var centuries = int.Parse(Console.ReadLine());
+            int years = centuries * yearsInCenturies;
+            int days = (int)(years * daysInYear);
+            int hours = days * hoursInDay;
+            int minutes = hours * minutesInHour;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
