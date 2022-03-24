1021dd162c4c3c1d41bf2ffa6207a47cb3a226cf
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-24 19:31:31 +0200

    Add files via upload

---
+++ Data Types and Variables - Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int currentNum = i;
+                int currNumSum = 0;
+
+                while (currentNum != 0)
+                {
+                    currNumSum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                bool isSpecial = currNumSum == 5 || currNumSum == 7 || currNumSum == 11;
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
---
+++ Data Types and Variables - Lab/07. Concat Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firts = Console.ReadLine();
+            string second = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firts}{delimiter}{second}");
+        }
+    }
+}
