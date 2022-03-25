f5e9f78e712aba71ce2e2735eb4394f27a34dae9
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-25 18:25:11 +0200

    Add files via upload

---
+++ Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int currPower = n;
+            int targets = 0;
+
+            while (currPower >= m)
+            {
+                currPower -= m;
+                targets++;
+
+                if (currPower == n * 0.5 && y != 0)
+                {
+                    currPower /= y;
+                }
+            }
+
+            Console.WriteLine(currPower);
+            Console.WriteLine(targets);
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int bestSnowballSnow = 0;
+            int bestSnowballTime = 0;
+            int bestSnowballQuality = 0;
+            System.Numerics.BigInteger bestSnowballValue = System.Numerics.BigInteger.Zero;
+
+            for (int i = 0; i < n; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                int snowballDivide = snowballSnow / snowballTime;
+                System.Numerics.BigInteger snowballValue = System.Numerics.BigInteger.Pow(snowballDivide, snowballQuality);
+
+                if (snowballValue >= bestSnowballValue)
+                {
+                    bestSnowballSnow = snowballSnow;
+                    bestSnowballTime = snowballTime;
+                    bestSnowballQuality = snowballQuality;
+                    bestSnowballValue = snowballValue;
+                }
+            }
+
+            Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestSnowballValue} ({bestSnowballQuality})");
+        }
+    }
+}
