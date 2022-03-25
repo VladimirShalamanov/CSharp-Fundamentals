e8af67dc97b46fb6312fcf63d9d36365929864a1
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-25 18:20:24 +0200

    Add files via upload

---
+++ Data Types and Variables - Exercise/1. Integer Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+
+            int sumOne = first + second;
+            int sumTwo = sumOne / third;
+            int sumThree = sumTwo * fourth;
+
+            Console.WriteLine(sumThree);
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/2. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nums = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                char digit = nums[i];
+                sum += int.Parse(digit.ToString());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
