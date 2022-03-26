06d8d2292063d58273617184c084f980b5f47f8e
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-26 22:51:06 +0200

    Add files via upload

---
+++ Data Types and Variables - More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string simbol = string.Empty;
+            int openCount = 0;
+            int closeCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                simbol = Console.ReadLine();
+
+                if (simbol == "(")
+                {
+                    openCount++;
+                }
+                else if (simbol == ")")
+                {
+                    closeCount++;
+
+                    if (openCount - closeCount != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+
+            if (openCount == closeCount)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
---
+++ Data Types and Variables - More Exercise/5. Decrypting Messages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _5._Decrypting_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string word = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                int letterNum = input + key;
+                char letter = (char)(letterNum);
+                word += letter.ToString();
+            }
+
+            Console.WriteLine(word); 
+        }
+    }
+}
