3170e2663556dd404a4946c15a1993b2050b911b
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-27 23:30:10 +0300

    Add files via upload

---
+++ Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int even = 0;
+            int odd = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int currNum = num[i];
+
+                if (currNum % 2 == 0)
+                {
+                    even += currNum;
+                }
+                else
+                {
+                    odd += currNum;
+                }
+            }
+
+            int difference = even - odd;
+
+            Console.WriteLine(difference);
+        }
+    }
+}
