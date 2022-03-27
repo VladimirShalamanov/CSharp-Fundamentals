b6bb99d0b465cba6f79401799d8575f8a9d6863d
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-27 23:35:47 +0300

    Add files via upload

---
+++ Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int first = nums.Length;
+
+            for (int i = 0; i < first - 1; i++)
+            {
+                int[] condensed = new int[nums.Length - 1];
+
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    condensed[j] = nums[j] + nums[j + 1];
+                }
+
+                nums = condensed;
+            }
+
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
