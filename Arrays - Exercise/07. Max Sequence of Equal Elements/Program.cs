8b98070cb1dad902d3655daa5168a65698bd2297
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-29 23:20:24 +0300

    Add files via upload

---
+++ Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = 0;
+            int winCount = 0;
+            int index = 0;
+            string num = string.Empty;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count++;
+
+                    if (count > winCount)
+                    {
+                        winCount = count;
+                        index = i;
+                        num = arr[i].ToString();
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+
+            for (int i = 0; i <= winCount; i++)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
---
+++ Arrays - Exercise/10. LadyBugs/10. LadyBugs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _10._LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] ladyBugField = new int[fieldSize];
+
+            string[] occupiedIndex = Console.ReadLine().Split();
+            for (int i = 0; i < occupiedIndex.Length; i++)
+            {
+                int currentIndex = int.Parse(occupiedIndex[i]);
+                if (currentIndex >= 0 && currentIndex < fieldSize)
+                {
+                    ladyBugField[currentIndex] = 1;
+                }
+            }
+
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "end")
+            {
+                bool isFirst = true;
+                int currIndex = int.Parse(command[0]);
+                while (currIndex >= 0 && currIndex < fieldSize && ladyBugField[currIndex] != 0)
+                {
+                    if (isFirst)
+                    {
+                        ladyBugField[currIndex] = 0;
+                        isFirst = false;
+                    }
+
+                    string direction = command[1];
+                    int flightLength = int.Parse(command[2]);
+                    if (direction == "left")
+                    {
+                        currIndex -= flightLength;
+                        if (currIndex >= 0 && currIndex < fieldSize)
+                        {
+                            if (ladyBugField[currIndex] == 0)
+                            {
+                                ladyBugField[currIndex] = 1;
+                                break;
+                            }
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        currIndex += flightLength;
+                        if (currIndex >= 0 && currIndex < fieldSize)
+                        {
+                            if (ladyBugField[currIndex] == 0)
+                            {
+                                ladyBugField[currIndex] = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine(String.Join(" ", ladyBugField));
+        }
+    }
+}
