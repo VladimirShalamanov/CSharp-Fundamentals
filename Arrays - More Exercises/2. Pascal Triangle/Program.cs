1ebe8fd70745d8092fb21bb0dd1b183aaa9a5275
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-03-30 23:38:09 +0300

    Add files via upload

---
+++ Arrays - More Exercises/2. Pascal Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[] currArr = { 1 };
+
+            for (int row = 1; row < rows; row++)
+            {
+                int[] newArr = new int[currArr.Length + 1];
+
+                for (int i = 0; i < currArr.Length; i++)
+                {
+                    newArr[i] += currArr[i];
+                    newArr[i + 1] = currArr[i];
+                }
+
+                Console.WriteLine(String.Join(' ', currArr));
+                currArr = newArr;
+            }
+
+            Console.WriteLine(String.Join(' ', currArr));
+        }
+    }
+}
---
+++ Arrays - More Exercises/4. Fold and Sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Fold_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int k = arr.Length / 4;
+
+            int[] lowerRow = new int[k * 2];
+            int lowerRowIndex = 0;
+
+            int[] upperRow = new int[k * 2];
+            int upperRowIndex = 0;
+
+            for (int i = k - 1; i >= 0; i--)
+            {
+                upperRow[upperRowIndex] = arr[i];
+                upperRowIndex++;
+            }
+
+            for (int i = arr.Length - 1; i > 3 * k - 1; i--)
+            {
+                upperRow[upperRowIndex] = arr[i];
+                upperRowIndex++;
+            }
+
+            Console.WriteLine();
+
+            for (int i = k; i < 3 * k; i++)
+            {
+                lowerRow[lowerRowIndex] = arr[i];
+                lowerRowIndex++;
+            }
+
+            for (int i = 0; i < upperRow.Length; i++)
+            {
+                Console.Write($"{upperRow[i] + lowerRow[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
