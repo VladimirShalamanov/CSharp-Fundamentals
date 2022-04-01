707801eb196169d0a1b999cdd0ac395d9d6cdad0
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-01 18:21:19 +0300

    Add files via upload

---
+++ Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int sumEvenNum = GetSumOfEvenDigits(input);
+            int sumOddsNum = GetSumOfOddNumber(input);
+
+            int result = EvenAndOdds(sumEvenNum, sumOddsNum);
+            Console.WriteLine(result);
+        }
+
+        static int EvenAndOdds(int evenNum, int oddNum)
+        {
+            int result = evenNum * oddNum;
+            return result;
+        }
+
+        static int GetSumOfOddNumber(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int currNum = number % 10;
+                number = number / 10;
+
+                if (currNum % 2 == 0)
+                {
+                    sum += currNum;
+                }
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int currNum = number % 10;
+                number = number / 10;
+
+                if (currNum % 2 != 0)
+                {
+                    sum += currNum;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Methods - Lab/11. Math Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11._Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string simbol = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            double end = Calculate(a, simbol, b);
+            Console.WriteLine(end);
+        }
+
+        static double Calculate(int a, string simbol, int b)
+        {
+            double result = 0;
+
+            switch (simbol)
+            {
+                case "/":
+                    result = a / b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
