7a1d2c3b453ef113fd0d7f3e04a29f4197ee6349
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-01 19:05:17 +0300

    Add files via upload

---
+++ Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Arrays___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string[] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+
+            if (day >= 1 && day <= days.Length)
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
