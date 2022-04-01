5f31fbb8bcf94fd18bba8f14ce0d2e7058e74a82
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-01 18:13:50 +0300

    Add files via upload

---
+++ Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double coffeePrice = 1.50;
+            double waterPrice = 1.00;
+            double cokePrice = 1.40;
+            double snacksPrice = 2.00;
+
+            switch (product)
+            {
+                case "coffee":
+                    coffee(quantity, coffeePrice);
+                    break;
+                case "water":
+                    water(quantity, waterPrice);
+                    break;
+                case "coke":
+                    coke(quantity, cokePrice);
+                    break;
+                case "snacks":
+                    snacks(quantity, snacksPrice);
+                    break;
+            }
+
+            static void coffee(int quantity, double coffeePrice)
+            {
+                Console.WriteLine($"{quantity * coffeePrice:f2}");
+            }
+            static void water(int quantity, double waterPrice)
+            {
+                Console.WriteLine($"{quantity * waterPrice:f2}");
+            }
+            static void coke(int quantity, double cokePrice)
+            {
+                Console.WriteLine($"{quantity * cokePrice:f2}");
+            }
+            static void snacks(int quantity, double snacksPrice)
+            {
+                Console.WriteLine($"{quantity * snacksPrice:f2}");
+            }
+        }
+    }
+}
