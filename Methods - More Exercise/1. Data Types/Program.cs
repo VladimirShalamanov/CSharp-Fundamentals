259acb9ad703fa7c80d6fe96a5c81017f9f763ad
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-10 21:11:34 +0300

    Add files via upload

---
+++ Methods - More Exercise/1. Data Types/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace More_Exercise___Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            GetResultType(type, input);
+        }
+
+        static void GetResultType(string type, string input)
+        {
+            if (type == "int")
+            {
+                int num = int.Parse(input);
+                num *= 2;
+                Console.WriteLine(num);
+            }
+            else if (type == "real")
+            {
+                double num = double.Parse(input);
+                num *= 1.5;
+                Console.WriteLine($"{num:f2}");
+            }
+            else if (type == "string")
+            {
+                string word = input;
+                Console.WriteLine($"${word}$");
+            }
+        }
+    }
+}
---
+++ Methods - More Exercise/5. Multiplication Sign/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _5._Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            GetAnswear(num1, num2, num3);
+        }
+
+        static void GetAnswear(int n1, int n2, int n3)
+        {
+            int cout = 0;
+            if (n1 == 0 || n2 == 0 || n3 == 0)
+            {
+                Console.WriteLine("zero");
+                return;
+            }
+            if (n1 < 0)
+            {
+                cout++;
+            }
+            if (n2 < 0)
+            {
+                cout++;
+            }
+            if (n3 < 0)
+            {
+                cout++;
+            }
+            if (cout % 2 == 0)
+            {
+                Console.WriteLine("positive");
+            }
+            if (cout % 2 != 0)
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
