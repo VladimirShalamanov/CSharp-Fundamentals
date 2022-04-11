48bb032542624ea04a527fb27a5da859941de58c
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-11 23:11:26 +0300

    Add files via upload

---
+++ Lists - Lab/2. Gauss' Trick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            int allChars = numbers.Count;
+
+            for (int i = 0; i < allChars / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
