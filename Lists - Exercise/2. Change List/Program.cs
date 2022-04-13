3ec24e523bacdfe9dca71d63f660b43788b68a3e
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-13 15:15:14 +0300

    Add files via upload

---
+++ Lists - Exercise/2. Change List/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] arr = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (arr.Length == 2)
+                {
+                    if (arr[0] == "Delete")
+                    {
+                        int numDelete = int.Parse(arr[1]);
+                        nums.Remove(numDelete);
+                    }
+                }
+                else if (arr.Length == 3)
+                {
+                    if (arr[0] == "Insert")
+                    {
+                        int element = int.Parse(arr[1]);
+                        int position = int.Parse(arr[2]);
+                        nums.Insert(position, element);
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
---
+++ Lists - Exercise/3. House Party/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> personGoing = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] name = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (name.Length == 3)
+                {
+                    if (personGoing.Contains(name[0]))
+                    {
+                        Console.WriteLine($"{name[0]} is already in the list!");
+                    }
+                    else if (!personGoing.Contains(name[0]))
+                    {
+                        personGoing.Add(name[0]);
+                    }
+                }
+                else if (name.Length == 4)
+                {
+                    if (!personGoing.Contains(name[0]))
+                    {
+                        Console.WriteLine($"{name[0]} is not in the list!");
+                    }
+                    else if (personGoing.Contains(name[0]))
+                    {
+                        personGoing.Remove(name[0]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < personGoing.Count; i++)
+            {
+                Console.WriteLine(personGoing[i]);
+            }
+        }
+    }
+}
