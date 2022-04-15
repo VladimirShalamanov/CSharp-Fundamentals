c175101578528a190d67f14a489add1513c5091f
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-15 18:27:23 +0300

    Add files via upload

---
+++ 1 - Preparation - Mid Exam Retake - Fundamentals/01. Computer Store/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            double priceWithoutTaxes = 0;
+            double taxes = 0;
+            double totalPrice = 0;
+
+            while (command != "regular" && command != "special")
+            {
+                double currPrice = double.Parse(command);
+
+                if (currPrice < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else if (currPrice > 0)
+                {
+                    priceWithoutTaxes += currPrice;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (priceWithoutTaxes == 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else if (command == "special")
+            {
+                taxes = priceWithoutTaxes * 0.2;
+                totalPrice = priceWithoutTaxes + taxes;
+                totalPrice *= 0.9;
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {priceWithoutTaxes:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalPrice:f2}$");
+            }
+            else if (command == "regular")
+            {
+                taxes = priceWithoutTaxes * 0.2;
+                totalPrice = priceWithoutTaxes + taxes;
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {priceWithoutTaxes:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalPrice:f2}$");
+            }
+        }
+    }
+}
---
+++ 1 - Preparation - Mid Exam Retake - Fundamentals/03. Memory Game/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Memory_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            int count = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                count++;
+                int[] arr = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int index1 = arr[0];
+                int index2 = arr[1];
+
+                if (index1 >= 0 && index2 <= nums.Count)
+                {
+                    int middleElement = nums.Count / 2;
+                    string elementAdd = $"-{count}a";
+                    nums.Insert(middleElement, elementAdd);
+                    nums.Insert(middleElement, elementAdd);
+                }
+                else if (nums[index1] == nums[index2])
+                {
+                    string guessElement = nums[index1];
+                    nums.RemoveAll(x => x == guessElement);
+                    Console.WriteLine($"Congrats! You have found matching elements - {guessElement}!");
+                }
+                else if (nums[index1] != nums[index2])
+                {
+                    Console.WriteLine($"Try again!");
+                }
+                if (nums.Count == 0)
+                {
+                    Console.WriteLine($"You have won in {count} turns!");
+                    break;
+                }
+            }
+
+            if (nums.Count > 0)
+            {
+                Console.WriteLine("Sorry you lose :(");
+                Console.WriteLine(string.Join(" ", nums));
+            }
+        }
+    }
+}
