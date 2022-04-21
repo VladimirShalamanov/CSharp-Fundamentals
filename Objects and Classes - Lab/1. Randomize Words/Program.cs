896394168639791cca11a0c2137c4f2890df5850
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-21 23:25:02 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/1. Randomize Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Random rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(0, words.Length);
+
+                var a = words[pos2];
+                var b = words[pos1];
+                words[pos2] = b;
+                words[pos1] = a;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
