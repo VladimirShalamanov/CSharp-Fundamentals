7b53e656c2ace53819bb5fd296b9b6b82641ff13
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-26 17:07:25 +0300

    Add files via upload

---
+++ Strings and Text Processing - Lab/4. Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                text = text.Replace(words[i], new string ('*', words[i].Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
