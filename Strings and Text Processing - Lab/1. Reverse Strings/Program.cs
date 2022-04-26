ea02d946b07eef17941ef5401255c45d2667f949
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-04-26 17:05:51 +0300

    Add files via upload

---
+++ Strings and Text Processing - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string reverse = string.Empty;
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    reverse += line[i];
+                }
+
+                Console.WriteLine($"{line} = {reverse}");
+            }
+        }
+    }
+}
---
+++ Strings and Text Processing - Lab/3. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string set = Console.ReadLine();
+            string work = Console.ReadLine();
+
+            while (work.Contains(set))
+            {
+                work = work.Replace(set, "");
+            }
+
+            Console.WriteLine(work);
+        }
+    }
+}
