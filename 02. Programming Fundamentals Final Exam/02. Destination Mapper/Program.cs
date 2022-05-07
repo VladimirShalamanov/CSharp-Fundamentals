0b1b979c69b80ced2ed5d3e3c7b8bc4d6be4dc1f
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-07 19:34:11 +0300

    Add files via upload

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string map = Console.ReadLine();
+
+            List<string> locations = new List<string>();
+
+            Regex regex = new Regex(@"(\=|\/)(?<destination>[A-Z][A-Za-z]{2,})(\1)");
+
+            int travelPoints = 0;
+
+            MatchCollection matches = regex.Matches(map);
+            foreach (Match match in matches)
+            {
+                string currDestionation = match.Groups["destination"].Value;
+
+                locations.Add(currDestionation);
+                travelPoints += currDestionation.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", locations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
