873acbeb360b93243a0b9ca23f3717442a7f244e
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-09 14:06:56 +0300

    Add files via upload

---
+++ 04. Programming Fundamentals Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Regex regex = new Regex(@"(\@\#{1,})(?<product>[A-Z][a-zA-Z0-9]{4,}[A-Z])(\@\#{1,})");
+
+            for (int i = 0; i < n; i++)
+            {
+                string barcode = Console.ReadLine();
+
+                Match match = regex.Match(barcode);
+
+                if (match.Success)
+                {
+                    string product = match.Groups["product"].Value;
+                    string group = string.Empty;
+
+                    for (int j = 0; j < product.Length; j++)
+                    {
+                        if (char.IsDigit(product[j]))
+                        {
+                            group += product[j];
+                        }
+                    }
+
+                    if (group == "")
+                    {
+                        group = "00";
+                    }
+
+                    Console.WriteLine($"Product group: {group}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
