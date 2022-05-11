3b64eefd938eb6e0577b373bfbffe432c0d24637
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-11 15:46:57 +0300

    Add files via upload

---
+++ Programming Fundamentals Final Exam - 03 April 2022/01. String Game/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._String_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Done")
+            {
+                string[] arr = cmd
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string type = arr[0];
+
+                if (type == "Change")
+                {
+                    char ch = char.Parse(arr[1]);
+                    char replace = char.Parse(arr[2]);
+                    text = text.Replace(ch, replace);
+                    Console.WriteLine(text);
+                }
+                else if (type == "Includes")
+                {
+                    string subs = arr[1];
+                    if (text.Contains(subs))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (type == "End")
+                {
+                    string subs = arr[1];
+                    string end = text.Substring(text.Length - subs.Length);
+
+                    if (end == subs)
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (type == "Uppercase")
+                {
+                    text = text.ToUpper();
+                    Console.WriteLine(text);
+                }
+                else if (type == "FindIndex")
+                {
+                    char ch = char.Parse(arr[1]);
+                    int index = text.IndexOf(ch);
+                    Console.WriteLine(index);
+                }
+                else if (type == "Cut")
+                {
+                    int start = int.Parse(arr[1]);
+                    int count = int.Parse(arr[2]);
+                    text = text.Substring(start, count);
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
